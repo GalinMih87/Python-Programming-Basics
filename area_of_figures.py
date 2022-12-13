4f8438100d1f11493a54b789784ef96c9f8abe81
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2022-12-13 18:26:30 +0200

    Update area_of_figures.py

--- area_of_figures.py
+++ area_of_figures.py
@@ -2,17 +2,17 @@ from math import pi
 
 type_of_the_figure = input()
 result = 0
-if type_of_the_figure == "square":
+if type_of_the_figure == "square":       # пресмята лице на квадрат
     side = float(input())
     result = side * side
-elif type_of_the_figure == "rectangle":
+elif type_of_the_figure == "rectangle":  # пресмята лице на правоъгълник
     said_a = float(input())
     said_b = float(input())
     result = said_a * said_b
-elif type_of_the_figure == "circle":
+elif type_of_the_figure == "circle":     # пресмята лице на кръг
     radius = float(input())
     result = pi * (radius ** 2)
-else:
+else:                                    # пресмята лице на триъгълник                
     side = float(input())
     height = float(input())
     result = side * height / 2
