d484666424aa14e813145a37867211eade93f2c2
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-06-05 11:40:31 +0300

    Update print_the_larger_number.py

--- print_the_larger_number.py
+++ print_the_larger_number.py
@@ -2,11 +2,6 @@ first_number = int(input())
 second_number = int(input())
 if first_number > second_number:
     print(first_number)
-# pss - РїСЂРѕРїСѓСЃРЅРё
-
-# if - Р°РєРѕ
-# elif - РѕСЃРІРµРЅ, Р°РєРѕ
-# else - РІСЃРёС‡РєРё РѕСЃС‚Р°РЅР°Р»Рё СЃР»СѓС‡Р°Рё
 elif second_number > first_number:
     print(second_number)
 else:
