7d16ba9620b2ca9d9bf17dd6beef09f75033cdbc
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-06-05 11:39:40 +0300

    Add files via upload

---
+++ print_the_larger_number.py
@@ -0,0 +1,13 @@
+first_number = int(input())
+second_number = int(input())
+if first_number > second_number:
+    print(first_number)
+# pss - РїСЂРѕРїСѓСЃРЅРё
+
+# if - Р°РєРѕ
+# elif - РѕСЃРІРµРЅ, Р°РєРѕ
+# else - РІСЃРёС‡РєРё РѕСЃС‚Р°РЅР°Р»Рё СЃР»СѓС‡Р°Рё
+elif second_number > first_number:
+    print(second_number)
+else:
+    print("Equal")
